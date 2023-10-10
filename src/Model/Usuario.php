69fa322e778f2598d652a0cc0e6bd7a81f02cd79
Gustavo André Richter <gustavoarichter@gmail.com>
2023-10-10 20:16:47 -0300

    wip: Ajuste na formatação e identação dos arquivos.

--- src/Model/Usuario.php
+++ src/Model/Usuario.php
@@ -2,18 +2,15 @@
 
 namespace Alura\Leilao\Model;
 
-class Usuario
-{
-    /** @var string */
-    private $nome;
+class Usuario {
+	/** @var string */
+	private $nome;
 
-    public function __construct(string $nome)
-    {
-        $this->nome = $nome;
-    }
+	public function __construct(string $nome) {
+		$this->nome = $nome;
+	}
 
-    public function getNome(): string
-    {
-        return $this->nome;
-    }
+	public function getNome(): string {
+		return $this->nome;
+	}
 }
